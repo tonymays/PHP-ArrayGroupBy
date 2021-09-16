bc2c097406eb614b4f456561a7452417bb2fd225
System Administrator <`````>
2021-09-16 12:40:29 -0500

    initial checkins

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # PHP-ArrayGroupBy
-A PHP class that reorganizes an array based upon a group by field which can them be used for summations, averages, listings, etc.
+A PHP class that provides array group by features such as counts, summations, averages, etc.
 
 
--- arrayGroupBy.php
+++ arrayGroupBy.php
@@ -2,17 +2,33 @@
 declare(strict_types = 1);
 
 class arrayGroupBy {
-	private $src = [];
+	private $array = [];
 
-	public function __construct(array $src) : void {
-		$this->src = $src;
+	public function __construct(array $array) {
+		$this->set($array);
 	}
 
-	public function __destruct() : void {
+	public function __destruct() {
 		$this->src = [];
 	}
 
+	public function set(array $array) : void {
+		if ($this->isValidMultiDimensional($array)) {
+			$this->array = $array;
+		} else {
+			throw new Exception("critical error: invalid array - must be an associaitve array");
+		}
+	}
+
+	public function get() : array {
+		return $this->array;
+	}
+
 	public function list() : array {
-		return $src;
+		return $this->array;
+	}
+
+	private function isValidMultiDimensional(array $array) : bool {
+		return (!(count($array) == count($array, COUNT_RECURSIVE)));
 	}
 }
